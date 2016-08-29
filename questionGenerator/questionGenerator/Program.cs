3b736d8d19f0b76d95683e390e9bda4d3540f81f
oscarmolin <oscar.molin@lund.ntig.se>
2016-08-29 09:46:03 +0200

    write Questions to file

---
+++ questionGenerator/questionGenerator/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace questionGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            List<Question> biologi = new List<Question>();
+            List<Question> geografi = new List<Question>();
+            List<Question> historia = new List<Question>();
+            List<Question> teknik = new List<Question>();
+            List<Question> tvspel = new List<Question>();
+            
+            biologi.Add(new Question("Vad?1","1","2","3","4"));
+            biologi.Add(new Question("Vad?2", "1", "2", "3", "4"));
+            biologi.Add(new Question("Vad?3", "1", "2", "3", "4"));
+            biologi.Add(new Question("Vad?4", "1", "2", "3", "4"));
+
+            
+            NewQuestions(biologi,"biologi");
+        }
+        public static void NewQuestions(List<Question> list2Write,string category)
+        {
+            string path = @"C:\quiz\";
+            
+
+            if (!File.Exists(path + "biologi" + ".txt"))
+            {
+                TextWriter tw = new StreamWriter(path + "biologi" + ".txt");
+                for (int i = 0; i < list2Write.Count; i++)
+                {
+                    list2Write[i].ShuffleAnswers();
+                    tw.WriteLine(list2Write[i].query);
+                    tw.WriteLine(list2Write[i].answers[0]);
+                    tw.WriteLine(list2Write[i].answers[1]);
+                    tw.WriteLine(list2Write[i].answers[2]);
+                    tw.WriteLine(list2Write[i].answers[3]);
+                   
+                }
+                tw.Close();
+            }
+            else
+            {
+                TextWriter tw = new StreamWriter(path + "biologi" + ".txt");
+
+                for (int i = 0; i < list2Write.Count; i++)
+                {
+                    tw.WriteLine(list2Write[i].query);
+                    tw.WriteLine(list2Write[i].answers[0]);
+                    tw.WriteLine(list2Write[i].answers[1]);
+                    tw.WriteLine(list2Write[i].answers[2]);
+                    tw.WriteLine(list2Write[i].answers[3]);
+                    
+                }
+                tw.Close();
+            }
+
+        }
+        public static void AddQuestions(List<Question> list2Write)
+        {
+            string path = @"C:\quiz\";
+
+            if (!File.Exists(path + "biologi" + ".txt"))
+            {
+                TextWriter tw = new StreamWriter(path + "biologi" + ".txt");
+                for (int i = 0; i < list2Write.Count; i++)
+                {
+                    list2Write[i].ShuffleAnswers();
+                    tw.WriteLine(list2Write[i].query);
+                    tw.WriteLine(list2Write[i].answers[0]);
+                    tw.WriteLine(list2Write[i].answers[1]);
+                    tw.WriteLine(list2Write[i].answers[2]);
+                    tw.WriteLine(list2Write[i].answers[3]);
+
+                }
+                tw.Close();
+            }
+            else
+            {
+                TextWriter tw = new StreamWriter(path + "biologi" + ".txt", true);
+
+                for (int i = 0; i < list2Write.Count; i++)
+                {
+                    tw.WriteLine(list2Write[i].query);
+                    tw.WriteLine(list2Write[i].answers[0]);
+                    tw.WriteLine(list2Write[i].answers[1]);
+                    tw.WriteLine(list2Write[i].answers[2]);
+                    tw.WriteLine(list2Write[i].answers[3]);
+
+                }
+                tw.Close();
+            }
+
+        }
+    }
+}
---
+++ questionGenerator/questionGenerator/Question.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionGenerator
+{
+
+    class Question
+    {
+        private Random r = new Random();
+        public string query;
+        private string rightAnswer { get; }
+        protected string wrongAnswer1 { get; }
+        protected string wrongAnswer2 { get; }
+        protected string wrongAnswer3 { get; }
+        public List<string> answers;
+        public Question(string query, string rightAnswer, string wrongAnswer1, string wrongAnswer2, string wrongAnswer3)
+        {
+            this.query = query;
+            this.rightAnswer = rightAnswer;
+            this.wrongAnswer1 = wrongAnswer1;
+            this.wrongAnswer2 = wrongAnswer2;
+            this.wrongAnswer3 = wrongAnswer3;
+            answers = new List<string>();
+            answers.Add(rightAnswer);
+            answers.Add(wrongAnswer1);
+            answers.Add(wrongAnswer2);
+            answers.Add(wrongAnswer3);
+
+           // answers = Scramble(answers);
+        }
+
+        private List<string> Scramble(List<string> list)
+        {
+            List<string> l = new List<string>();
+            l.Add(list[0]);
+            l.Add(list[1]);
+            l.Add(list[2]);
+            l.Add(list[3]);
+               
+            for(int i = 0; i < list.Count; i++)
+            {
+                
+                int temp2 = r.Next(0,l.Count);
+                
+                list[l.Count-1] = l[temp2];
+
+                l.RemoveAt(temp2);
+                
+
+            }
+
+            return (list);
+        }
+        public void ShuffleAnswers()
+        {
+            answers = Scramble(answers);
+        }
+
+    }
+}
